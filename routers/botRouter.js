ffc77d7ef78c9268060ae63535bc0d0d91e2147e
Arian <arianlearning@gmail.com>
2025-10-02 14:42:10 +0200

    Reorganizar archivos a nuevas carpetas

--- routers/botRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { BotController } = require("../controllers/botController");
-const BotRouter = Router();
-
-BotRouter.post("/generate-review", BotController.generateReview);
-
-module.exports = {
-  BotRouter,
-};
